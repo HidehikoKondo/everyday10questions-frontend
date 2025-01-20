7e219b833ad91746d9761e7788ceb6cd71dc6941
Hidehiko Kondo <dev@udonko.net>
2025-01-21 00:12:24 +0900

    メニューの挙動実装中

--- script.js
+++ script.js
@@ -27,5 +27,20 @@ function fetchGASData() {
 
 
 // 関数を実行
-fetchGASData();
+// fetchGASData();
 
+
+// fnを使うための設定
+window.fn = {};
+
+window.fn.open = function () {
+    var menu = document.getElementById('menu');
+    menu.open();
+};
+
+window.fn.load = function (page) {
+    var content = document.getElementById('content');
+    var menu = document.getElementById('menu');
+    content.load(page)
+        .then(menu.close.bind(menu));
+};
